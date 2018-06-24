148ca5f313129a098302f11369eca172cb84e85c
Hubertus van Dam <hvandam@bnl.gov>
2018-06-24 11:39:28 -0700

    HVD: Do Tau stunts after configuring GA.

--- bin/nwc_tau_f90.sh
+++ bin/nwc_tau_f90.sh
@@ -15,18 +15,15 @@
 if [ ${#USE_BRAIN} -eq 0 ]; then
   export USE_BRAIN="yes"
   if [ ${#USE_TAU} -eq 0 ]; then
-    echo "HVD: call none" > /dev/stderr
     ${TAU_F90_ABSOLUTE} -g $@
   else
     # TAU_MAKEFILE has been hacked to ensure absolute pathnames for the compilers
     #export TAU_MAKEFILE=/global/homes/v/vandam/bin/Makefile.tau-intel-mpi-pdt
     if [ ${USE_TAU} == "source" ]; then
       # export F90=/opt/cray/pe/craype/2.5.12/bin/ftn
-      echo "HVD: call source" > /dev/stderr
       tau_f90.sh -g $@
     elif [ ${USE_TAU} == "compiler" ]; then
       # export F90=/opt/cray/pe/craype/2.5.12/bin/ftn
-      echo "HVD: call compiler" > /dev/stderr
       tauf90 -g $@
     else
       echo "Unknown USE_TAU: " $USE_TAU

2e240d808caecfd2545292ca4b37fb5545bd1d77
root <choij@ornl.gov>
2018-11-08 22:27:52 +0000

    Fix for using DATASPACES

--- test.sh
+++ test.sh
@@ -17,6 +17,48 @@ export TAU_SOS_CACHE_DEPTH=100
 export SOS_PUB_CACHE_DEPTH=100
 export TAU_VERBOSE=1
 
+# clean-up previous run
+rm -f log.* core* conf dataspaces.conf
+
+SERVER=/Install/dataspaces-1.7.2/bin/dataspaces_server
+
+WRITEPROC=1
+STAGINGPROC=1
+READPROC=1
+let "PROCALL=WRITEPROC+READPROC"
+
+# Prepare config file for DataSpaces
+echo "## Config file for DataSpaces
+ndim = 2
+dims = 100,100
+max_versions = 10
+#max_readers =" $READPROC "
+#lock_type = 2
+" > dataspaces.conf
+
+# Run DataSpaces
+echo "-- Start DataSpaces server "$SERVER" on $STAGINGPROC PEs, -s$STAGINGPROC -c$PROCALL"
+$SERVER -s$STAGINGPROC -c$PROCALL >& log.server &
+
+## Give some time for the servers to load and startup
+sleep 1s
+while [ ! -f conf ]; do
+    echo "-- File conf is not yet available from server. Sleep more"
+    sleep 1s
+done
+sleep 3s  # wait server to fill up the conf file
+
+## Export the main server config to the environment
+while read line; do
+    export set "${line}"
+done < conf
+
+echo "-- DataSpaces IDs: P2TNID = $P2TNID   P2TPID = $P2TPID"
+
+# Start STAGE_READ
+echo "-- Start GENARRAY on $READPROC PEs"
+python3 ./adios_read_mpi.py DATASPACES "verbose=3;check_read_status=0" >& log.reader &
+
 # The ADIOS extraction script has to be launched on the same node as 
 # one of the SOS aggregators, so the "extract" script is used to launch
 # both from Slurm (using --gres-network didn't seem to work)
@@ -61,7 +103,7 @@ export TAU_SOS_TRACING=1
 export TAU_PROFILE_FORMAT=merged
 export TAU_PROFILE_PREFIX=nwchem
 
-. ./etc/profile
+. /etc/profile
 
 cd QA/tests/ethanol
 

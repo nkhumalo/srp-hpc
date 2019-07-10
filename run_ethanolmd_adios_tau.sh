3b3a517424b7fbfef14a0c826359409e071fc4ae
root <hasungsoo@gmail.com>
2019-07-10 20:48:53 +0000

    test BP and ADIOS with MDTrajAnal code

--- run_ethanolmd_adios_tau.sh
+++ run_ethanolmd_adios_tau.sh
@@ -10,7 +10,12 @@ cd QA/tests/ethanol
 sed -i 's/coord 0/coord 1/' ethanol_md.nw
 sed -i 's/scoor 0/scoor 1/' ethanol_md.nw
 
-mpirun -n 2 ../../../bin/LINUX64/nwchem ethanol_md.nw
+export PYTHONPATH=${PYTHONPATH}:/opt/adios2/lib/python3.5/site-packages
+export PYTHONPATH=${PYTHONPATH}:/MDTrAnal/lib
+mpirun --allow-run-as-root -n 10 python3 /MDTrAnal/src/md_compress_adaptive_adios2.py &
+sleep 10
 
-bpls -l nwchem_xyz.bp
+mpirun --allow-run-as-root -n 2 ../../../bin/LINUX64/nwchem ethanol_md.nw
+
+#bpls -l nwchem_xyz.bp
 
